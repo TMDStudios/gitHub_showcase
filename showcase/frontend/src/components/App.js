abf67a03232bcb0667567fe8480f3f53351cc406
TMD Studios <tmdstudioscontact@gmail.com>
2021-04-03 18:16:01 +0400

    Finished React Setup

--- showcase/frontend/src/components/App.js
+++ showcase/frontend/src/components/App.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+import Header from './layout/Header';
+import Dashboard from './projects/Dashboard';
 
 class App extends Component{
     render(){
-        return <h1>YO YO</h1>
+        return (
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+            </Fragment>
+        )
     }
 }
 
