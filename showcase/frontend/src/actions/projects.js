f37025fcdbf3c915eb766d18471cbc68230b8758
TMD Studios <tmdstudioscontact@gmail.com>
2021-04-13 20:44:02 +0400

    Cleaned code

--- showcase/frontend/src/actions/projects.js
+++ showcase/frontend/src/actions/projects.js
@@ -1,36 +1,49 @@
-import axios from 'axios';
-import { createMessage, returnErrors } from './messages';
+import axios from "axios";
+import { createMessage, returnErrors } from "./messages";
 
-import { GET_PROJECTS, DELETE_PROJECT, ADD_PROJECT } from './types';
+import { GET_PROJECTS, DELETE_PROJECT, ADD_PROJECT } from "./types";
 
 // Get Projects
-export const getProjects = () => dispatch => {
-    axios.get('/api/projects/').then(res => {
-        dispatch({
-            type: GET_PROJECTS,
-            payload: res.data
-        });
-    }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
+export const getProjects = () => (dispatch) => {
+  axios
+    .get("/api/projects/")
+    .then((res) => {
+      dispatch({
+        type: GET_PROJECTS,
+        payload: res.data,
+      });
+    })
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 // Delete Project
-export const deleteProject = id => dispatch => {
-    axios.delete(`/api/projects/${id}/`).then(res => {
-        dispatch(createMessage({ deleteProject: 'Project Deleted' }));
-        dispatch({
-            type: DELETE_PROJECT,
-            payload: id
-        });
-    }).catch(err => console.log(err));
+export const deleteProject = (id) => (dispatch) => {
+  axios
+    .delete(`/api/projects/${id}/`)
+    .then((res) => {
+      dispatch(createMessage({ deleteProject: "Project Deleted" }));
+      dispatch({
+        type: DELETE_PROJECT,
+        payload: id,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 // Add Project
-export const addProject = project => dispatch => {
-    axios.post('/api/projects/', project).then(res => {
-        dispatch(createMessage({ addProject: 'Project Added' }));
-        dispatch({
-            type: ADD_PROJECT,
-            payload: res.data
-        });
-    }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
+export const addProject = (project) => (dispatch) => {
+  axios
+    .post("/api/projects/", project)
+    .then((res) => {
+      dispatch(createMessage({ addProject: "Project Added" }));
+      dispatch({
+        type: ADD_PROJECT,
+        payload: res.data,
+      });
+    })
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
