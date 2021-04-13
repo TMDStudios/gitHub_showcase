ab795c96269cab8920dd765413a03fe56adf7bb2
TMD Studios <tmdstudioscontact@gmail.com>
2021-04-13 20:41:16 +0400

    Added login/logout functionality

--- showcase/frontend/src/actions/auth.js
+++ showcase/frontend/src/actions/auth.js
@@ -1,37 +1,104 @@
-import axios from 'axios';
-import { returnErrors } from './messages';
+import axios from "axios";
+import { returnErrors } from "./messages";
 
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from './types';
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+} from "./types";
 
 // Check token and load user
 export const loadUser = () => (dispatch, getState) => {
-    // User loading
-    dispatch({ type: USER_LOADING });
+  // User loading
+  dispatch({ type: USER_LOADING });
 
-    // Get token from state
-    const token = getState().auth.token;
+  // Get token from state
+  const token = getState().auth.token;
 
-    // Headers
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    // If token, add to headers config
-    if(token){
-        config.headers['Authorization'] = `Token ${token}`;
-    }
+  // If token, add to headers config
+  if (token) {
+    config.headers["Authorization"] = `Token ${token}`;
+  }
 
-    axios.get('/api/auth/user', config).then(res => {
-        dispatch({
-            type: USER_LOADED,
-            payload: res.data
-        });
-    }).catch(err => {
-        dispatch(returnErrors(err.response.data, err.response.status));
-        dispatch({
-            type: AUTH_ERROR
-        });
+  axios
+    .get("/api/auth/user", config)
+    .then((res) => {
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: AUTH_ERROR,
+      });
     });
-}
+};
+
+// Login user
+export const login = (username, password) => (dispatch) => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  // Request body
+  const body = JSON.stringify({ username, password });
+
+  axios
+    .post("/api/auth/login", body, config)
+    .then((res) => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+    });
+};
+
+// Logout user
+export const logout = () => (dispatch, getState) => {
+  // Get token from state
+  const token = getState().auth.token;
+
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  // If token, add to headers config
+  if (token) {
+    config.headers["Authorization"] = `Token ${token}`;
+  }
+
+  axios
+    .post("/api/auth/logout/", null, config)
+    .then((res) => {
+      dispatch({
+        type: LOGOUT_SUCCESS,
+      });
+    })
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+    });
+};
--- showcase/frontend/src/components/layout/Header.js
+++ showcase/frontend/src/components/layout/Header.js
@@ -1,29 +1,76 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
 
 export class Header extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <div className="container">
-                    <div className="container-fluid">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
-                        data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" 
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand" href="#">GitHub Showcase</a>
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item"><Link to="/register" className="nav-link">Register</Link></li>
-                            <li className="nav-item"><Link to="/login" className="nav-link">Login</Link></li>
-                        </ul>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
+  static PropTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <button
+            className="nav-link btn btn-info btn-sm text-light"
+            onClick={this.props.logout}
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <div className="container">
+          <div className="container-fluid">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarTogglerDemo01"
+              aria-controls="navbarTogglerDemo01"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <a className="navbar-brand" href="#">
+                GitHub Showcase
+              </a>
+            </div>
+            {isAuthenticated ? authLinks : guestLinks}
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Header);
